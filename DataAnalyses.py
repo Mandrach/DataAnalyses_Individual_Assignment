50e3baf04358cba064aa3702773105100d32cb5b
Mandrach <daniel_amg@web.de>
2020-01-16 15:20:29 +0100

    Added headline to my csv file

--- DataAnalyses.py
+++ DataAnalyses.py
@@ -16,7 +16,7 @@ with open ('.\data\conflict_data_full_lined.json') as json_file:
 
 with open('israel_data.csv', 'w', encoding='utf8') as file:
         #for loop to get the whole entry
+    file.write(f'Id,Country,Year,best_fatality_guess,active_year,type_of_violence,event_clarity \n')
     for entry in data:
         file.write(f'{entry["id"]},{entry["country"]},{entry["year"]},{entry["best"]},{entry["active_year"]},{entry["type_of_violence"]},{entry["event_clarity"]}\n')
-   
-
+    
