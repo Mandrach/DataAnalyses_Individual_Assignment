54912211d1ce106861ca3e8cec7a14b28e9d1731
Mandrach <daniel_amg@web.de>
2020-01-16 14:59:12 +0100

    Removed the source_Article from the csv file
    The source is a huge string with different inter punctuation, therefore very difficult to work with for R

--- DataAnalyses.py
+++ DataAnalyses.py
@@ -8,7 +8,7 @@ with open ('.\data\conflict_data_full_lined.json') as json_file:
     # Id, 
     # Country, 
     # year, 
-    # best, 
+    # best [best guess for fatalities]
     # active_year, 
     # type_of_violence, 
     # source_article, 
@@ -17,5 +17,6 @@ with open ('.\data\conflict_data_full_lined.json') as json_file:
 with open('israel_data.csv', 'w', encoding='utf8') as file:
         #for loop to get the whole entry
     for entry in data:
-        file.write(f'{entry["id"]},{entry["country"]},{entry["year"]},{entry["best"]},{entry["active_year"]}, {entry["type_of_violence"]},{entry["event_clarity"]},{entry["source_article"]}\n')
+        file.write(f'{entry["id"]},{entry["country"]},{entry["year"]},{entry["best"]},{entry["active_year"]}, {entry["type_of_violence"]},{entry["event_clarity"]}\n')
+   
 
