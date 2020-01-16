9524a62b7395bc1260f94e7d7796b375502bea05
Mandrach <daniel_amg@web.de>
2020-01-16 14:47:44 +0100

    Added lines for converting the json data file into a csv file

--- DataAnalyses.py
+++ DataAnalyses.py
@@ -1,11 +1,21 @@
-import data
+import json
 
-with open 'conflict_data_full_lined.json' as json_file:
-    data = json_file.load()
 
-    total_fatalities = 0
-    country = Israel
-    year = 2001
+with open ('.\data\conflict_data_full_lined.json') as json_file:
+    data = json.load(json_file)
 
+    # Which attribues do I want and need?:
+    # Id, 
+    # Country, 
+    # year, 
+    # best, 
+    # active_year, 
+    # type_of_violence, 
+    # source_article, 
+    # event_clarity
+
+with open('israel_data.csv', 'w', encoding='utf8') as file:
+        #for loop to get the whole entry
     for entry in data:
-        if entry[year] == 2001 and entry[] 
+        file.write(f'{entry["id"]},{entry["country"]},{entry["year"]},{entry["best"]},{entry["active_year"]}, {entry["type_of_violence"]},{entry["event_clarity"]},{entry["source_article"]}\n')
+
